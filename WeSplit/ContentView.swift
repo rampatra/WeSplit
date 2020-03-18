d83496ce99a317a4b05d4b8c67869e7982ea5129
Ram Patra <ramswarooppatra@gmail.com>
2020-03-18 18:31:07 +0000

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ram Patra on 18/03/2020.
+//  Copyright © 2020 Ram Patra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
